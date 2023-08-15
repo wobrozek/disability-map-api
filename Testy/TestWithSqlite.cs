b649a84a41465e7b7cf69f0643dd3e5656d8e278
Wojciech Brożek <wobrozek@gmail.com>
2023-08-15 17:21:10 +0200

    add sqlite databse to tests

---
+++ Testy/TestWithSqlite.cs
@@ -0,0 +1,29 @@
+﻿using disability_map.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Testy
+{
+    public class TestWithSqlite : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly DbMainContext DbContext;
+
+        protected TestWithSqlite()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<DbMainContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            DbContext = new DbMainContext(options);
+            DbContext.Database.EnsureCreated();
+        }
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+    }
+}
