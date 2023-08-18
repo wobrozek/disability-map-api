4f127a11480ca370e44d765e0411e7f689ad79f0
Wojtek Brozek <wobrozek@gmail.com>
2023-08-18 14:10:20 +0200

    add optional parametr DeleteFromQueue

---
+++ disability-map/Repositories/AuthRepository/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using disability_map.Dtos;
+using disability_map.Models;
+
+namespace disability_map.Data
+{
+    public interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<bool> FieldExists(string username, string field);
+        Task<ServiceResponse<UserDataDto>> Login(string login, string password);
+    }
+}
